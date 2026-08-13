7e6a1764b9533636c3b5d10fe692e8e244f21a6d
Bananas021k <bananas021k@gmail.com>
2026-08-13 17:23:27 +0300

    Improved softmax

--- FowardPassOOP.py
+++ FowardPassOOP.py
@@ -12,16 +12,18 @@ class Layer:
         self.w = np.random.rand(*w) * 0.01 # *w to unload the tuple, input needs 2 numbers
         self.b = np.zeros((b)) 
 
-    def Forward(self, data):
+    def forward(self, data):
          y = data @ self.w + self.b
          return y
     
-def Relu(y):
+def relu(y):
     y = np.maximum(0,y)
     return y  
 
 
 def SoftMax(x):
+    largestnum_x = np.max(x, axis=1, keepdims=True)
+    x = x - largestnum_x
     x = np.exp(x)
     sums = np.sum(x, axis=1, keepdims=True)
     x = x / sums
@@ -29,17 +31,17 @@ def SoftMax(x):
     return x
 
 l1 = Layer((64,32), (1,32))
-y1 = l1.Forward(norm_data)
+y1 = l1.forward(norm_data)
 
-y1 = Relu(y1)
+y1 = relu(y1)
 
 l2 = Layer((32,16), (1,16))
-y2 = l2.Forward(y1)
+y2 = l2.forward(y1)
 
-y2 = Relu(y2)
+y2 = relu(y2)
 
 l3 = Layer((16,10), (1,10))
-y3 = l3.Forward(y2)
+y3 = l3.forward(y2)
 
 probs = SoftMax(y3)
 
@@ -57,7 +59,7 @@ def create_prediction(index):
 # -- DISPLAY IMAGE --
 def display_image(index, prediction, answer):
     plt.imshow(digits.images[index], cmap='gray')
-    plt.title(f"Prediction: {prediction} Answer: {answer}")
+    plt.title(f"Prediction: {prediction} Actual: {answer}")
     plt.show()
 
 
@@ -67,11 +69,13 @@ def display_predictions(prediction, predictions_probs, answer):
     categories = np.arange(0, 10) # Generates numbers from 0 to 9
 
     plt.bar(categories , predictions_probs)
-    plt.title(f"Prediction: {prediction} Answer: {answer}")
+    plt.title(f"Prediction: {prediction} Actual: {answer}")
 
     plt.show()
+
 
 index = int(input("index: "))
 prediction_probs, prediction, answer = create_prediction(index)
 display_image(index, prediction, answer)
 display_predictions(prediction, prediction_probs, answer)
+
