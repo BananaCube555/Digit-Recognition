4743bdb5a11ca26c298382fd00158290dda7431d
Bananas021k <bananas021k@gmail.com>
2026-08-19 16:41:09 +0300

    Turned loss calc into a function

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -80,18 +80,18 @@ def display_predictions(prediction, predictions_probs, answer):
 # display_image(index, prediction, answer)
 # display_predictions(prediction, prediction_probs, answer)
 
+def Loss_Calculation():
+    
+    correct_class_preds = []
 
-correct_class_preds = []
+    for index in range(1797):
+        current_image_probs = probs[index]
+        ans = digits.target[index]
+        pred_for_ans = current_image_probs[ans]
 
-for index in range(1797):
-    current_image_probs = probs[index]
-    ans = digits.target[index]
-    pred_for_ans = current_image_probs[ans]
+        correct_class_preds.append(pred_for_ans)
 
-    correct_class_preds.append(pred_for_ans)
+        # pred_for_ans = (probs[index])[(digits.target[index])]
 
-    # pred_for_ans = (probs[index])[(digits.target[index])]
-
-losses = -np.log(correct_class_preds)
-
-print(losses)
+    losses = -np.log(correct_class_preds)
+    return losses
